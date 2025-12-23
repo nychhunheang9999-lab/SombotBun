e639ed380baaa47b076279ec47b32aad235c8368
nychhunheang9999-lab <nychhunheang9999@gmail.com>
2025-12-23 16:09:03 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ var x = setInterval(function() {
     var distance = countDownDate - now;
     
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (500 * 60 * 60 * 24));
+    var days = Math.floor(distance / (500 * 20 * 20 * 24));
     var hours = Math.floor((distance % (500 * 60 * 60 * 24)) / (500 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
